334794d18971e1a4f15888f2380908a1bf404231
CodeAnd-Art <3838yos4141@gmail.com>
2026-07-28 15:49:18 +0300

    Update modul_muzik.ps1

--- modul_muzik.ps1
+++ modul_muzik.ps1
@@ -1,7 +1,14 @@
-# music_3.mp3 dosyasını aynı klasörden çalar
+# 30 saniyelik müziği SÜREKLİ TEKRARLA
 $player = New-Object -ComObject WMPlayer.OCX
 $player.settings.volume = 100
+$player.settings.autoStart = $true
 $player.URL = ".\music_3.mp3"
-$player.controls.play()
 
-while ($true) { Start-Sleep -Seconds 10 }
+while ($true) {
+    $player.controls.play()
+    # Müzik bitene kadar bekle (30 sn veya dosya uzunluğu kadar)
+    Start-Sleep -Seconds 30
+    # Müzik bittiğinde yeniden başlat (tekrar)
+    $player.controls.stop()
+    Start-Sleep -Milliseconds 100
+}
