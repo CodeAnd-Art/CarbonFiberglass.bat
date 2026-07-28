62a63edd0328005668ef21b96d0a427c43d92b1f
CodeAnd-Art <3838yos4141@gmail.com>
2026-07-28 14:19:13 +0300

    Create modul_ekran_ters.ps1

---
+++ modul_ekran_ters.ps1
@@ -0,0 +1,20 @@
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+$form = New-Object System.Windows.Forms.Form
+$form.WindowState = 'Maximized'
+$form.FormBorderStyle = 'None'
+$form.TopMost = $true
+$form.BackColor = 'Black'
+
+$label = New-Object System.Windows.Forms.Label
+$label.Dock = 'Fill'
+$label.Font = New-Object System.Drawing.Font('Consolas', 100, [System.Drawing.FontStyle]::Bold)
+$label.ForeColor = 'Red'
+$label.TextAlign = 'MiddleCenter'
+$label.Text = '⚠️ EKRAN DÖNDÜ ⚠️'
+$form.Controls.Add($label)
+
+try { $form.RotateFlip = 1 } catch {}
+
+$form.ShowDialog()
